cca009d3d2813e504876bd61729cb6ea425dacbd
Aruna Tebel <arunautebel@gmail.com>
2023-12-17 18:36:48 +0530

    Fix styling issues

--- src/background.js
+++ src/background.js
@@ -4,12 +4,14 @@
   chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     if (tab.url) {
       const url = new URL(tab.url);
-      await chrome.tabs.sendMessage(tabId, {
-        href: url.href,
-        host: url.host,
-        origin: url.origin,
-        title: tab.title,
-      });
+      try {
+        await chrome.tabs.sendMessage(tabId, {
+          href: url.href,
+          host: url.host,
+          origin: url.origin,
+          title: tab.title,
+        });
+      } catch (error) {}
     }
   });
 })();
