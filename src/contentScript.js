5072509629588b0ccd544c3ac7a1029e54555c5e
Aruna Tebel <arunautebel@gmail.com>
2023-12-17 13:11:17 +0530

    Revamp the project to use chrome-extension-cli for scaffolding

---
+++ src/contentScript.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const moment = require('moment');
+
+function getUrlPathWithoutOrigin(url) {
+  return url.href.split(url.origin)[1];
+}
+
+function getDocumentData() {
+  let descrption = document.querySelector('meta[name="description"]')?.content;
+  if (!descrption) {
+    descrption = document.querySelector('meta[name="Description"]')?.content;
+  }
+  if (!descrption) {
+    descrption = document.querySelector(
+      'meta[property="og:description"]'
+    )?.content;
+  }
+  return {
+    descrption,
+  };
+}
+
+function addVisitedUrl() {
+  const url = new URL(window.location.href);
+  updateStorage(
+    url.host,
+    url.href,
+    getUrlPathWithoutOrigin(url),
+    document.title
+  );
+}
+
+addVisitedUrl();
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  updateStorage(
+    message.host,
+    message.href,
+    getUrlPathWithoutOrigin(message),
+    message.title
+  );
+});
+
+function updateStorage(host, url, urlPath, title) {
+  chrome.storage.local.get(['pages.visited']).then((result) => {
+    let pagesVisited = result['pages.visited'];
+    if (!pagesVisited) {
+      pagesVisited = {
+        [host]: { [url]: {} },
+      };
+    } else if (!pagesVisited[host]) {
+      pagesVisited[host] = { [url]: {} };
+    }
+    const documentData = getDocumentData();
+    pagesVisited[host][url] = {
+      dt: moment().unix(),
+      t: title,
+      d: documentData.descrption,
+      p: urlPath,
+    };
+
+    chrome.storage.local.set({ 'pages.visited': pagesVisited }).then();
+  });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}", "index.html", "scripts/index.js"],
-	theme: {
-		colors: {
-			primary: "#093d65",
-			secondary: "#60d394",
-			yellow: "#ffd97d",
-			green: "#aaf683",
-			red: "#e86558",
-			"arch-gray": "#f6f6f6",
-			"arch-black": "##262626",
-			"arch-gray-1": "#595959",
-			"arch-gray-2": "#8a8a8a",
-			white: "#ffffff",
-			black: "#000000",
-		},
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{html,js}', './public/popup.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#093d65',
+        secondary: '#60d394',
+        yellow: '#ffd97d',
+        green: '#aaf683',
+        red: '#e86558',
+        'arch-gray': '#f6f6f6',
+        'arch-black': '##262626',
+        'arch-gray-1': '#595959',
+        'arch-gray-2': '#8a8a8a',
+        white: '#ffffff',
+        black: '#000000',
+      },
+    },
+  },
+  plugins: [],
 };
