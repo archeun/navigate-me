c464c16d5c806992e6d33cd7cf9f437bb4342969
Aruna Tebel <arunautebel@gmail.com>
2023-12-17 18:15:07 +0530

    Wrap background script inside IIAF

--- src/background.js
+++ src/background.js
@@ -1,13 +1,15 @@
 'use strict';
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (tab.url) {
-    const url = new URL(tab.url);
-    await chrome.tabs.sendMessage(tabId, {
-      href: url.href,
-      host: url.host,
-      origin: url.origin,
-      title: tab.title,
-    });
-  }
-});
+(function () {
+  chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if (tab.url) {
+      const url = new URL(tab.url);
+      await chrome.tabs.sendMessage(tabId, {
+        href: url.href,
+        host: url.host,
+        origin: url.origin,
+        title: tab.title,
+      });
+    }
+  });
+})();
